1987b07c569b81b3fc8535c6714fb5fc20c71030
Dipankar Mazumdar <103004148+dipankarmazumdar@users.noreply.github.com>
2025-11-18 17:10:48 -0500

    existing_delta_uniform

---
+++ ch07/existing_delta_uniform.sql
@@ -0,0 +1,8 @@
+ALTER TABLE existing_delta_table
+SET TBLPROPERTIES (
+ 'delta.minReaderVersion' = '2',
+ 'delta.minWriterVersion' = '5',
+
+  'delta.enableIcebergCompatV2' = 'true', 
+  'delta.universalFormat.enabledFormats' = 'iceberg
+);
