c5270237ee1ccf59a86c8451b0cd59f338b9b8a6
Dipankar Mazumdar <103004148+dipankarmazumdar@users.noreply.github.com>
2025-11-18 17:09:52 -0500

    delta_uniform

---
+++ ch07/delta_uniform.sql
@@ -0,0 +1,12 @@
+CREATE TABLE delta_uniform_table (
+  id INT,
+  name STRING,
+  age INT,
+  city STRING
+)
+USING DELTA
+TBLPROPERTIES (
+  'delta.enableIcebergCompatV2' = 'true', 
+  'delta.universalFormat.enabledFormats' = 'iceberg
+)
+LOCATION 's3://my-delta-uniform-table/';
